151ec7537b521d2c34b5eda1d73571bd58ef2af9
Legyumin <66237249+Legyumin@users.noreply.github.com>
2020-07-14 14:47:32 +0900

    2020 07 14

---
+++ Assets/ScrollingObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObejct: MonoBehaviour{  
+      public float speed = 10f;
+    
+    private void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
